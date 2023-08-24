4c79c04fd4ef52063696cb09f781def389a5bf8f
ApharMagaramova <meherremova.afer2003@gmail.com>
2023-08-24 23:55:35 +0400

    pagination added

--- WebUI/Pages/Blog.cshtml.cs
+++ WebUI/Pages/Blog.cshtml.cs
@@ -27,12 +27,16 @@ namespace WebUI.Pages
 
         public List<CategoryDto>? Categories { get; set; }
 
+        public int CurrentPageIndex { get; set; }
+
+        public int TotalPostCount { get; set; }
+
         public BlogModel(IMediator mediator)
         {
             _mediator = mediator;
         }
 
-        public async Task OnGetAsync(short language)
+        public async Task OnGetAsync(short language, int pageIndex = 1)
         {
             SelectedLanguageId = language;
             Categories = await _mediator.Send(new GetCategoriesQuery());
@@ -47,12 +51,12 @@ namespace WebUI.Pages
                 }
             }
 
-
             Posts = await _mediator.Send(new GetPostsQuery());
-
+                        
             // Fetch post translations for each post
-            foreach (var post in Posts)
+            for (int i = Posts.Count - 1; i >= 0; i--)
             {
+                var post = Posts[i];
                 try
                 {
                     post.PostTranslation = await _mediator.Send(new GetPostTranslationByLanguageIdAndNewsIdQuery
@@ -63,15 +67,24 @@ namespace WebUI.Pages
                 }
                 catch (Exception e)
                 {
-                    post.PostTranslation = null;
+                    Posts.RemoveAt(i);
                 }
             }
+
+            TotalPostCount = Posts.Count;
+            var pageSize = 5; // You can adjust the page size as needed
+            var itemsToSkip = (pageIndex - 1) * pageSize;
+
+            // Fetch posts for the current page
+            Posts = Posts.Skip(itemsToSkip).Take(pageSize).ToList();
+            // Set the current page index for pagination links
+            CurrentPageIndex = pageIndex;
         }
 
         public Task<IActionResult> OnPostAsync()
         {
             // Reload the page with the selected LanguageId
-            return Task.FromResult<IActionResult>(RedirectToPage("/blog", new { language = SelectedLanguageId }));
+            return Task.FromResult<IActionResult>(RedirectToPage("/blog", new { language = SelectedLanguageId, pageIndex = 1 }));
         }
     }
 }
